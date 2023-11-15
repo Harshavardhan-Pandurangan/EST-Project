886bc033b2c999e51df02252652f1cce6cc4053e
Harshavardhan-Pandurangan <rockingharsha71@gmail.com>
2023-11-15 14:21:23 +0530

    maps in country stats completed

--- src/pages/CountryDetails.js
+++ src/pages/CountryDetails.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { Grid } from "@mui/material";
@@ -23,7 +24,11 @@ export default function CountryDetails(props) {
                 2000: "/assets/images/2000_ivory.svg",
                 2013: "/assets/images/2013_ivory.svg",
             },
-            forest: {},
+            forest: {
+                1975: "/assets/images/1975_forest_ivory.png",
+                2000: "/assets/images/2000_forest_ivory.png",
+                2013: "/assets/images/2013_forest_ivory.png",
+            },
         },
         liberia: {
             general: {
@@ -31,7 +36,11 @@ export default function CountryDetails(props) {
                 2000: "/assets/images/2000_liberia.svg",
                 2013: "/assets/images/2013_liberia.svg",
             },
-            forest: {},
+            forest: {
+                1975: "/assets/images/1975_forest_liberia.png",
+                2000: "/assets/images/2000_forest_liberia.png",
+                2013: "/assets/images/2013_forest_liberia.png",
+            },
         },
         senegal: {
             general: {
@@ -39,7 +48,11 @@ export default function CountryDetails(props) {
                 2000: "/assets/images/2000_senegal.svg",
                 2013: "/assets/images/2013_senegal.svg",
             },
-            forest: {},
+            forest: {
+                1975: "/assets/images/1975_forest_senegal.png",
+                2000: "/assets/images/2000_forest_senegal.png",
+                2013: "/assets/images/2013_forest_senegal.png",
+            },
         },
         sierra: {
             general: {
@@ -47,7 +60,11 @@ export default function CountryDetails(props) {
                 2000: "/assets/images/2000_sierra.svg",
                 2013: "/assets/images/2013_sierra.svg",
             },
-            forest: {},
+            forest: {
+                1975: "/assets/images/1975_forest_sierra.png",
+                2000: "/assets/images/2000_forest_sierra.png",
+                2013: "/assets/images/2013_forest_sierra.png",
+            },
         },
     };
 
@@ -107,119 +124,135 @@ export default function CountryDetails(props) {
     }, [year, type]);
 
     return (
-        <div
-            style={{
-                backgroundColor: "#F2FFE9",
-                height: "100vh",
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "column",
-                justifyContent: "center",
-                width: "100%",
-            }}
-        >
-            <div
+        <>
+            <Button
+                variant="contained"
                 style={{
-                    width: "80%",
-                    textAlign: "center",
-                    marginBottom: "6vh",
+                    margin: "2vh",
+                    position: "absolute",
+                    zIndex: 100,
+                    backgroundColor: "#557C55",
+                }}
+                onClick={() => {
+                    navigate("/");
                 }}
             >
-                <Grid
-                    container
+                Back to Home
+            </Button>
+            <div
+                style={{
+                    position: "relative", // Set position to relative for the parent container
+                    backgroundColor: "#F2FFE9",
+                    height: "100vh",
+                    display: "flex",
+                    alignItems: "center",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    width: "100%",
+                }}
+            >
+                <div
                     style={{
-                        backgroundColor: "#A6CF98",
-                        borderRadius: 20,
-                        margin: 0,
+                        width: "80%",
+                        textAlign: "center",
+                        marginBottom: "6vh",
                     }}
-                    width={"100%"}
                 >
                     <Grid
-                        item
-                        xs={12}
-                        md={4}
+                        container
                         style={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center",
-                        }}
-                    >
-                        {/* capitalise the country's first letter */}
-                        <Typography variant="h3">
-                            {country.charAt(0).toUpperCase() + country.slice(1)}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <Typography variant="h2">Blah</Typography>
-                    </Grid>
-                    <Grid
-                        item
-                        xs={12}
-                        md={4}
-                        style={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "center",
-                        }}
-                    >
-                        <Typography variant="h3">{year}</Typography>
-                    </Grid>
-                </Grid>
-            </div>
-
-            <div style={{ width: "90%", height: "70%" }}>
-                <Grid
-                    container
-                    spacing={2}
-                    style={{
-                        backgroundColor: "#A6CF98",
-                        borderRadius: 20,
-                        margin: 0,
-                    }}
-                    height={"100%"}
-                    width={"100%"}
-                >
-                    <Grid
-                        item
-                        xs={12}
-                        md={3.7}
-                        style={{
-                            backgroundColor: "#557C55",
+                            backgroundColor: "#A6CF98",
                             borderRadius: 20,
-                            margin: 8,
-                            color: "white",
+                            margin: 0,
                         }}
+                        width={"100%"}
                     >
-                        Reasoning goes here
-                        {/* use the country, type and states to show data */}
-                    </Grid>
-                    <Grid item xs={12} md={4.2}>
-                        <img
-                            src={image_path}
-                            alt="Country map"
+                        <Grid
+                            item
+                            xs={12}
+                            md={4}
                             style={{
-                                width: "90%",
-                                height: "90%",
-                                objectFit: "contain",
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
                             }}
-                        />
+                        >
+                            <Typography variant="h3">
+                                {country.charAt(0).toUpperCase() +
+                                    country.slice(1)}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <Typography variant="h2">Blah</Typography>
+                        </Grid>
+                        <Grid
+                            item
+                            xs={12}
+                            md={4}
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+                            <Typography variant="h3">{year}</Typography>
+                        </Grid>
                     </Grid>
+                </div>
+                <div style={{ width: "90%", height: "70%" }}>
                     <Grid
-                        item
-                        xs={12}
-                        md={3.8}
+                        container
+                        spacing={2}
                         style={{
-                            backgroundColor: "#557C55",
+                            backgroundColor: "#A6CF98",
                             borderRadius: 20,
-                            margin: 8,
-                            color: "white",
+                            margin: 0,
                         }}
+                        height={"100%"}
+                        width={"100%"}
                     >
-                        Cumulative graphs go here
-                        {/* use the country, year and type states to show data */}
+                        <Grid
+                            item
+                            xs={12}
+                            md={3.7}
+                            style={{
+                                backgroundColor: "#557C55",
+                                borderRadius: 20,
+                                margin: 8,
+                                color: "white",
+                            }}
+                        >
+                            Reasoning goes here
+                            {/* use the country, type and states to show data */}
+                        </Grid>
+                        <Grid item xs={12} md={4.2}>
+                            <img
+                                src={image_path}
+                                alt="Country map"
+                                style={{
+                                    width: "90%",
+                                    height: "90%",
+                                    objectFit: "contain",
+                                }}
+                            />
+                        </Grid>
+                        <Grid
+                            item
+                            xs={12}
+                            md={3.8}
+                            style={{
+                                backgroundColor: "#557C55",
+                                borderRadius: 20,
+                                margin: 8,
+                                color: "white",
+                            }}
+                        >
+                            Cumulative graphs go here
+                            {/* use the country, year and type states to show data */}
+                        </Grid>
                     </Grid>
-                </Grid>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
