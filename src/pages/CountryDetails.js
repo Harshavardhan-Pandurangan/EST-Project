963b26357448beb036876417b3ee0e27e90feeab
sarthakchittawar <sarthak.chittawar@research.iiit.ac.in>
2023-11-20 15:45:34 +0530

    graph function updated

--- src/pages/CountryDetails.js
+++ src/pages/CountryDetails.js
@@ -5,6 +5,8 @@ import Button from "@mui/material/Button";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { Grid } from "@mui/material";
+import * as d3 from 'd3'
+import tippy from 'tippy.js'
 
 export default function CountryDetails(props) {
     const { country_param, year_param, type_param } = useParams();
@@ -16,6 +18,15 @@ export default function CountryDetails(props) {
     const [year, setYear] = useState(1975);
     const [type, setType] = useState("general");
     const [image_path, setImagePath] = useState("");
+    const [rendercount, setRendercount] = useState(0);
+    const [reason, setReason] = useState("");
+
+    const reasons = {
+        ivory: "/assets/images/ivory.png",
+        liberia: "/assets/images/liberia.png",
+        senegal: "/assets/images/senegal.png",
+        sierra: "/assets/images/sierra.png",
+    };
 
     const image_paths = {
         ivory: {
@@ -73,7 +84,13 @@ export default function CountryDetails(props) {
         setYear(year_param);
         setType(type_param);
 
+        // d3.select("#svgid0").remove()
+        d3.select("#svgid1").remove()
+        d3.select("#svgid2").remove()
+        setRendercount(0)
+
         setImagePath(image_paths[country_param][type_param][year_param]);
+        setReason(reasons[country_param]);
     }, [country_param, year_param, type_param]);
 
     // listen for key clicks
@@ -84,6 +101,9 @@ export default function CountryDetails(props) {
         const handleKeyDown = (e) => {
             switch (e.key) {
                 case "ArrowLeft":
+                    d3.select("#svgid0").remove()
+                    d3.select("#svgid1").remove()
+                    setRendercount(0)
                     let prevYear = (years.indexOf(+year) + 2) % 3;
                     navigate(
                         `/country_stats/${country}/${
@@ -92,6 +112,9 @@ export default function CountryDetails(props) {
                     );
                     break;
                 case "ArrowRight":
+                    d3.select("#svgid0").remove()
+                    d3.select("#svgid1").remove()
+                    setRendercount(0)
                     let nextYear = (years.indexOf(+year) + 1) % 3;
                     navigate(
                         `/country_stats/${country}/${
@@ -100,12 +123,18 @@ export default function CountryDetails(props) {
                     );
                     break;
                 case "ArrowUp":
+                    d3.select("#svgid0").remove()
+                    d3.select("#svgid1").remove()
+                    setRendercount(0)
                     let nextType = (types.indexOf(type) + 1) % 2;
                     navigate(
                         `/country_stats/${country}/${year}/${types[nextType]}`
                     );
                     break;
                 case "ArrowDown":
+                    d3.select("#svgid0").remove()
+                    d3.select("#svgid1").remove()
+                    setRendercount(0)
                     let prevType = (types.indexOf(type) + 1) % 2;
                     navigate(
                         `/country_stats/${country}/${year}/${types[prevType]}`
@@ -114,7 +143,10 @@ export default function CountryDetails(props) {
                 default:
                     break;
             }
+            e.preventDefault()
         };
+
+        graphs();
 
         window.addEventListener("keydown", handleKeyDown);
 
@@ -122,6 +154,157 @@ export default function CountryDetails(props) {
             window.removeEventListener("keydown", handleKeyDown);
         };
     }, [year, type]);
+
+    var m;
+    var colours;
+    var margin = {top: 50, right: 0, bottom: 60, left: 50},
+            width = 300 - margin.left - margin.right,
+            height = 300 - margin.top - margin.bottom;
+    useEffect(() => {
+
+    // remove svg if exists
+    // if (loaded){
+    // d3.select("#svgid0").remove()
+    var svg = d3.select("#my_dataviz")
+    .append("svg")
+        // .style("background-color", "#A6CF98")
+        .attr("width", width + margin.left + margin.right + 80)
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("id", `svgid${rendercount}`)
+    .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+    console.log(svg)
+    m = new Map()
+    colours = new Map()
+    colours.set("forest", 'darkgreen')
+    colours.set("grassland", 'lightgreen')
+    colours.set("desert", 'brown')
+    colours.set("settlements", 'red')
+    colours.set("agriculture", 'yellow')
+    colours.set("water bodies", 'blue')
+    colours.set("degraded forest", 'purple')
+    m.set("forest", 0)
+    m.set("grassland", 0)
+    m.set("desert", 0)
+    m.set("settlements", 0)
+    m.set("agriculture", 0)
+    m.set("water bodies", 0)
+    m.set("degraded forest", 0)
+
+    // setLoaded(true)}
+    setRendercount(rendercount+1)
+
+    // graphs();
+
+    }, [year, type, country]);
+
+    
+    const graphs = async function ()
+    {
+        console.log(year)
+        m = new Map()
+        m.set("forest", 0)
+        m.set("grassland", 0)
+        m.set("desert", 0)
+        m.set("settlements", 0)
+        m.set("agriculture", 0)
+        m.set("water bodies", 0)
+        m.set("degraded forest", 0)
+
+        var svg = d3.select("#svgid0")
+        const data = await d3.csv(`https://raw.githubusercontent.com/Harshavardhan-Pandurangan/EST-Project/main/src/dataset/${country}.csv`)
+        var categories = []
+
+        // const year = 2000
+        const data2 = []
+        var scale = 0.00001
+
+        for(var i=0; i<data.length; i++)
+        {             
+            if (!categories.includes(data[i]["Category"]))
+            {
+                categories.push(data[i]["Category"])
+            }
+            data2.push({"Category": data[i]["Category"], "Area": scale * parseFloat(data[i][`Area-${year}`]), "Perc": parseFloat(data[i][`Perc-${year}`])})
+
+        }
+
+        var round = 1
+        const y = d3.scaleBand()
+                .range([100, 100+width])
+                .domain(categories.map(function(d) { return d }))
+                .padding(0.2)
+
+            
+            svg.append("g")
+                .attr("id", "xaxis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(y))
+                .selectAll("text")
+                    .attr("transform", "translate(-10,0)rotate(-45)")
+                    .style("text-anchor", "end");
+
+const max_area = d3.max(data2, function(d) { return d["Area"]; }) / scale
+            const x = d3.scaleLinear()
+                .domain([0, max_area])
+                .range([ height, 0])
+            svg.append("g")
+                .attr("id", "yaxis")
+                .attr("transform", "translate(100,0)")
+                .call(d3.axisLeft(x))
+            
+
+            var bar = svg.selectAll("mybar")
+                .data(data2)
+                .enter()
+                .append("rect")
+                    .attr("id", function(d) {return "c"+categories.indexOf(d["Category"])})
+                    .attr("x", function(d) { return y(d['Category']); })
+                    .attr("width", y.bandwidth())
+                    .attr("fill", function(d) {return colours.get(d["Category"].toLowerCase());})
+                    // no bar at the beginning thus:
+                    .attr("height", function(d) {return m.get(d["Category"].toLowerCase()); }) // always equal to 0
+                    .attr("y", function(d) {return height - m.get(d["Category"].toLowerCase()); })
+            bar.transition()
+                .duration(1000)
+                .attr("y", function(d) { m.set(d["Category"].toLowerCase(), parseFloat(d["Area"]*height));return height - d["Area"]/scale * height/max_area; })
+                .attr("height", function(d) { return d["Area"]/scale * height/max_area; })
+                .delay(function(d,i){return(0*100)})
+
+            bar.on("mouseover", function (d, f) {
+                tippy("#c"+categories.indexOf(f["Category"]), {
+                    content: "Category: "+f["Category"]+"\nArea: "+Math.round(f["Area"]/scale)+"\nPercentage: "+Math.round(f["Perc"])
+                })
+            })
+            
+            svg.append("text")
+            .attr("id", "xtitle")
+            .attr("text-anchor", "end")
+            .attr("x", width/2 + 120)
+            .attr("y", height + margin.top + 35)
+            .text("Land classes");
+
+            svg.append("text")
+            .attr("id", "overtext")
+            .attr("text-anchor", "center")
+            .attr("x", width/2 + 60)
+            .attr("y", margin.top)
+
+            // Y axis label:
+            svg.append("text")
+                .attr("id", "ytitle")
+                .attr("text-anchor", "end")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 30)
+                .attr("x", -30)
+                .text("Area (in sq. km)")
+
+            if (data2.length != 0)
+            {
+                svg.selectAll("#overtext").text("Year: "+year)
+            }
+    }
 
     return (
         <>
@@ -183,7 +366,7 @@ export default function CountryDetails(props) {
                             </Typography>
                         </Grid>
                         <Grid item xs={12} md={4}>
-                            <Typography variant="h2">Blah</Typography>
+                            <Typography variant="h2">Deforestation in West Africa</Typography>
                         </Grid>
                         <Grid
                             item
@@ -200,7 +383,7 @@ export default function CountryDetails(props) {
                     </Grid>
                 </div>
                 <div style={{ width: "90%", height: "70%" }}>
-                    <Grid
+                <Grid
                         container
                         spacing={2}
                         style={{
@@ -222,8 +405,29 @@ export default function CountryDetails(props) {
                                 color: "white",
                             }}
                         >
-                            Reasoning goes here
+                            <img
+                                    src="/assets/legend.png"
+                                    style={{
+                                        zIndex: 10,
+                                        width: "12%",
+                                        height: "15%",
+                                        objectFit: "contain",
+                                        position: "absolute",
+                                        marginLeft: "50%",
+                                    }}
+                                />
+                            {/*Reasoning goes here
                             {/* use the country, type and states to show data */}
+                            <p style={{padding: 20, fontSize: 20}}>Causes of deforestation in {country.charAt(0).toUpperCase() + country.slice(1)}:</p>
+                            <img
+                                src={reason}
+                                alt="Country reason map"
+                                style={{
+                                    width: "90%",
+                                    height: "90%",
+                                    objectFit: "contain",
+                                }}
+                            />
                         </Grid>
                         <Grid item xs={12} md={4.2}>
                             <img
@@ -247,7 +451,7 @@ export default function CountryDetails(props) {
                                 color: "white",
                             }}
                         >
-                            Cumulative graphs go here
+                            <div id="my_dataviz"></div>
                             {/* use the country, year and type states to show data */}
                         </Grid>
                     </Grid>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,10 +5,14 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { Grid } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import * as d3 from 'd3'
+import tippy from 'tippy.js'
 
 export default function LandingPage() {
     const [year, setYear] = useState(1975);
     const [type, setType] = useState("general");
+    const [country, setCountry] = useState("west_africa");
+    const [rendercount, setRendercount] = useState(0);
 
     let navigate = useNavigate();
 
@@ -17,16 +21,31 @@ export default function LandingPage() {
         const handleKeyDown = (e) => {
             switch (e.key) {
                 case "ArrowLeft":
-                    setYear(year - 1);
+                    d3.select("#svgid0").remove()
+                    d3.select("#svgid1").remove()
+                    setRendercount(0)
+                    if (year == 1975) setYear(2013);
+                    else if (year == 2000) setYear(1975);
+                    else if (year == 2013) setYear(2000)
                     break;
                 case "ArrowRight":
-                    setYear(year + 1);
+                    d3.select("#svgid0").remove()
+                    d3.select("#svgid1").remove()
+                    setRendercount(0)
+                    if (year == 1975) setYear(2000);
+                    else if (year == 2000) setYear(2013);
+                    else if (year == 2013) setYear(1975)
                     break;
                 case "ArrowUp":
+                    d3.select("#svgid0").remove()
+                    d3.select("#svgid1").remove()
+                    setRendercount(0)
                     if (type === "general") setType("forest");
                     else setType("general");
                     break;
                 case "ArrowDown":
+                    d3.select("#svgid0").remove()
+                    d3.select("#svgid1").remove()
                     if (type === "general") setType("forest");
                     else setType("general");
                     break;
@@ -35,12 +54,166 @@ export default function LandingPage() {
             }
         };
 
+        graphs();
+
         window.addEventListener("keydown", handleKeyDown);
 
         return () => {
             window.removeEventListener("keydown", handleKeyDown);
         };
-    }, [year, type]);
+    }, [year, type, country]);
+
+    var m;
+    var colours;
+    var margin = {top: 50, right: 0, bottom: 60, left: 50},
+            width = 300 - margin.left - margin.right,
+            height = 300 - margin.top - margin.bottom;
+    useEffect(() => {
+
+    // remove svg if exists
+    // if (loaded){
+    // d3.select("#svgid0").remove()
+    var svg = d3.select("#my_dataviz")
+    .append("svg")
+        // .style("background-color", "#A6CF98")
+        .attr("width", width + margin.left + margin.right + 150)
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("id", `svgid${rendercount}`)
+    .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+    console.log(svg)
+    m = new Map()
+    colours = new Map()
+    colours.set("forest", 'darkgreen')
+    colours.set("grassland", 'lightgreen')
+    colours.set("desert", 'brown')
+    colours.set("settlements", 'red')
+    colours.set("agriculture", 'yellow')
+    colours.set("water bodies", 'blue')
+    colours.set("degraded forest", 'purple')
+    m.set("forest", 0)
+    m.set("grassland", 0)
+    m.set("desert", 0)
+    m.set("settlements", 0)
+    m.set("agriculture", 0)
+    m.set("water bodies", 0)
+    m.set("degraded forest", 0)
+
+    // setLoaded(true)}
+    setRendercount(rendercount+1)
+
+    // graphs();
+
+    }, [year, type, country]);
+
+    
+    const graphs = async function ()
+    {
+        console.log(year)
+        m = new Map()
+        m.set("forest", 0)
+        m.set("grassland", 0)
+        m.set("desert", 0)
+        m.set("settlements", 0)
+        m.set("agriculture", 0)
+        m.set("water bodies", 0)
+        m.set("degraded forest", 0)
+
+        var svg = d3.select("#svgid0")
+        const data = await d3.csv(`https://raw.githubusercontent.com/Harshavardhan-Pandurangan/EST-Project/main/src/dataset/${country}.csv`)
+        var categories = []
+
+        // const year = 2000
+        const data2 = []
+        var scale = 0.00001
+
+        for(var i=0; i<data.length; i++)
+        {             
+            if (!categories.includes(data[i]["Category"]))
+            {
+                categories.push(data[i]["Category"])
+            }
+            data2.push({"Category": data[i]["Category"], "Area": scale * parseFloat(data[i][`Area-${year}`]), "Perc": parseFloat(data[i][`Perc-${year}`])})
+
+        }
+
+        var round = 1
+        const y = d3.scaleBand()
+                .range([100, 100+width])
+                .domain(categories.map(function(d) { return d }))
+                .padding(0.2)
+
+            
+            svg.append("g")
+                .attr("id", "xaxis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(y))
+                .selectAll("text")
+                    .attr("transform", "translate(-10,0)rotate(-45)")
+                    .style("text-anchor", "end");
+
+const max_area = d3.max(data2, function(d) { return d["Area"]; }) / scale
+            const x = d3.scaleLinear()
+                .domain([0, max_area])
+                .range([ height, 0])
+            svg.append("g")
+                .attr("id", "yaxis")
+                .attr("transform", "translate(100,0)")
+                .call(d3.axisLeft(x))
+            
+
+            var bar = svg.selectAll("mybar")
+                .data(data2)
+                .enter()
+                .append("rect")
+                    .attr("id", function(d) {return "c"+categories.indexOf(d["Category"])})
+                    .attr("x", function(d) { return y(d['Category']); })
+                    .attr("width", y.bandwidth())
+                    .attr("fill", function(d) {return colours.get(d["Category"].toLowerCase());})
+                    // no bar at the beginning thus:
+                    .attr("height", function(d) {return m.get(d["Category"].toLowerCase()); }) // always equal to 0
+                    .attr("y", function(d) {return height - m.get(d["Category"].toLowerCase()); })
+            bar.transition()
+                .duration(1000)
+                .attr("y", function(d) { m.set(d["Category"].toLowerCase(), parseFloat(d["Area"]*height));return height - d["Area"]/scale * height/max_area; })
+                .attr("height", function(d) { return d["Area"]/scale * height/max_area; })
+                .delay(function(d,i){return(0*100)})
+
+            bar.on("mouseover", function (d, f) {
+                tippy("#c"+categories.indexOf(f["Category"]), {
+                    content: "Category: "+f["Category"]+"\nArea: "+Math.round(f["Area"]/scale)+"\nPercentage: "+Math.round(f["Perc"])
+                })
+            })
+            
+            svg.append("text")
+            .attr("id", "xtitle")
+            .attr("text-anchor", "end")
+            .attr("x", width/2 + 120)
+            .attr("y", height + margin.top + 35)
+            .text("Land classes");
+
+            svg.append("text")
+            .attr("id", "overtext")
+            .attr("text-anchor", "center")
+            .attr("x", width/2 + 60)
+            .attr("y", margin.top)
+
+            // Y axis label:
+            svg.append("text")
+                .attr("id", "ytitle")
+                .attr("text-anchor", "end")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 30)
+                .attr("x", -30)
+                .text("Area (in sq. km)")
+
+            if (data2.length != 0)
+            {
+                svg.selectAll("#overtext").text("Year: "+year)
+            }
+    }
+
 
     const image_paths = {
         ivory: {
@@ -112,7 +285,7 @@ export default function LandingPage() {
                     marginBottom: "6vh",
                 }}
             >
-                <Typography variant="h2">Blah</Typography>
+                <Typography variant="h2">Deforestation in West Africa</Typography>
             </div>
 
             <div style={{ width: "80%", height: "70%" }}>
@@ -143,6 +316,17 @@ export default function LandingPage() {
                                     height: "100%",
                                 }}
                             >
+                                <img
+                                    src="/assets/legend.png"
+                                    style={{
+                                        zIndex: 10,
+                                        width: "17%",
+                                        height: "30%",
+                                        objectFit: "contain",
+                                        position: "absolute",
+                                        marginLeft: "80%",
+                                    }}
+                                />
                                 <img
                                     src={image_paths["ivory"][type][year]}
                                     style={{
@@ -249,7 +433,7 @@ export default function LandingPage() {
                         </Grid>
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        Cumulative graphs go here
+                    <div id="my_dataviz" style={{'p': 10}}></div>
                         {/* use the year and type states to show data */}
                     </Grid>
                 </Grid>
